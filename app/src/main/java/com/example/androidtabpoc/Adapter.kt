c91d3d7d311f500b142bd0e1d51ae7fd6952dba1
Tom <itagaki.tomoya@eversense.co.jp>
2020-04-09 19:05:13 +0900

    文字色の変更

--- app/src/main/java/com/example/androidtabpoc/Adapter.kt
+++ app/src/main/java/com/example/androidtabpoc/Adapter.kt
@@ -1,12 +1,23 @@
 package com.example.androidtabpoc
 
 import android.content.Context
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.BackgroundColorSpan
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
 class Adapter(fragmentManager: FragmentManager, private val context: Context):
     FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val tabItems = arrayOf("ホーム","かわいい","あるある","笑える","なるほど", "泣ける")
+    private val tabColors = arrayOf("#FF889A","#FA963F","#F7BB36","#62BFBF","#8490D6","#72ACD8")
 
     override fun getItem(position: Int): Fragment {
         return when(position) {
@@ -20,14 +31,14 @@ class Adapter(fragmentManager: FragmentManager, private val context: Context):
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return when(position) {
-            0 -> "ホーム"
-            1 -> "かわいい"
-            2 -> "あるある"
-            3 -> "笑える"
-            4 -> "なるほど"
-            5 -> "泣ける"
-            else -> "その他"
-        }
+        val spannable = SpannableStringBuilder(tabItems[position])
+        spannable.setSpan(
+                ForegroundColorSpan(Color.parseColor(tabColors[position])),
+                0, // start
+                tabItems[position].length, // end
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+
+        return spannable
     }
 }
--- app/src/main/java/com/example/androidtabpoc/OtherTabFragment.kt
+++ app/src/main/java/com/example/androidtabpoc/OtherTabFragment.kt
@@ -8,19 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [HomeTabFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [HomeTabFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OtherTabFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,21 +18,10 @@ class OtherTabFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeTabFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeTabFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
